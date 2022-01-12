8f6249d9adb5f6be7db5915560db24144bf55e56
Alberto Campini <alberto.campini@edu.unito.it>
2022-01-12 19:29:50 +0100

    User microservice Update -Implementata API per gestire meglio il login con FB Gestione keepalive del token

--- user-microservice/src/main/java/esameTAAS/userMicroservice/Repositories/AccessTokenRepository.java
+++ user-microservice/src/main/java/esameTAAS/userMicroservice/Repositories/AccessTokenRepository.java
@@ -1,6 +1,7 @@
 package esameTAAS.userMicroservice.Repositories;
 
 import esameTAAS.userMicroservice.Models.AccessToken;
+import esameTAAS.userMicroservice.Models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -13,6 +14,9 @@ public interface AccessTokenRepository extends JpaRepository<AccessToken,String>
 
     @Query("SELECT T FROM AccessToken T WHERE T.token = :token")
     AccessToken findAccessTokenByToken(@Param("token") String token);
+
+    @Query("SELECT U FROM User U JOIN AccessToken T ON U.username = T.username WHERE T.token = :token")
+    User findUserByToken(@Param("token") String token);
 
 
 
